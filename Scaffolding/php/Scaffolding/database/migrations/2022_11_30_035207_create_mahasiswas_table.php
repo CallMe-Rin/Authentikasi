9fb0cd7cf1c224efb8e7f35d8903d2f186926c59
Rinaldo Dwi Faturahman <faturahmanrinaldo@gmail.com>
2022-12-15 18:49:31 +0700

    Laravel Authentifikasi

---
+++ Scaffolding/php/Scaffolding/database/migrations/2022_11_30_035207_create_mahasiswas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->char('nim')->primary();
+            $table->char('nama');
+            $table->integer('umur');
+            $table->char('alamat');
+            $table->char('kota');
+            $table->char('kelas');
+            $table->char('jurusan');
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
